38c046989a1bccdc2d20d0a3cbb2fefa8ad4c0ea
nikola9888 <151091217+nikola9888@users.noreply.github.com>
2026-07-22 19:23:36 +0200

    Refactor AboutScreen to use translations and ModernButton

--- screens/about.py
+++ screens/about.py
@@ -1,9 +1,11 @@
 from kivy.uix.screenmanager import Screen
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
-from kivy.uix.button import Button
+from kivy.app import App
 
+from components.button import ModernButton
 from theme import TEXT, TITLE
+from translations import translations
 
 
 class AboutScreen(Screen):
@@ -11,6 +13,7 @@ class AboutScreen(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
+        self.current_language = App.get_running_app().language
 
         root = BoxLayout(
             orientation="vertical",
@@ -19,8 +22,9 @@ class AboutScreen(Screen):
         )
 
 
-        title = Label(
-            text=" WalletCore",
+        # TITLE
+        self.title = Label(
+            text=translations[self.current_language]["about"],
             color=TEXT,
             font_size=TITLE,
             bold=True,
@@ -29,38 +33,59 @@ class AboutScreen(Screen):
         )
 
 
-        info = Label(
-            text=(
-                "WalletCore\n\n"
-                "Smart personal finance manager.\n\n"
-                "Track income, expenses and budgets "
-                "in one simple application.\n\n"
-                "Version 1.0\n\n"
-                "Developed by DEVELOPMENT4WORLD\n\n"
-                "E-mail: development4world@gmail.com"
-            ),
+        # INFO
+        self.info = Label(
+            text=self.get_about_text(),
             color=TEXT,
-            font_size=32
+            font_size=28
         )
 
 
-        back = Button(
-            text="← Back",
+        # BACK
+        self.back = ModernButton(
+            text=translations[self.current_language]["back"],
             size_hint_y=None,
-            height=60
+            height=100
         )
 
-        back.bind(
+        self.back.bind(
             on_press=self.go_back
         )
 
 
-        root.add_widget(title)
-        root.add_widget(info)
-        root.add_widget(back)
-
+        root.add_widget(self.title)
+        root.add_widget(self.info)
+        root.add_widget(self.back)
 
         self.add_widget(root)
+
+        self.bind(on_enter=self.update_language)
+
+
+    def get_about_text(self):
+
+        t = translations[self.current_language]
+
+        return (
+            "WalletCore\n\n"
+            f"{t['smart_finance_manager']}\n\n"
+            f"{t['track_money']}\n\n"
+            f"{t['version']} 1.0\n\n"
+            f"{t['developed_by']}\n\n"
+            "E-mail: development4world@gmail.com"
+        )
+
+
+
+    def update_language(self ,*args):
+
+        self.current_language = App.get_running_app().language
+
+        t = translations[self.current_language]
+
+        self.title.text = t["about"]
+        self.info.text = self.get_about_text()
+        self.back.text = t["back"]
 
 
 
